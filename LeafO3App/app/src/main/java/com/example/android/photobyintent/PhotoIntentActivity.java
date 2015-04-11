972fba463b1180053b0b50a0428f45f465c4a8cd
Omar Bougamza <omar.bougamza@gmail.com>
2015-04-12 00:58:00 +0100

    new features: taking a picture saving it processing it

--- LeafO3App/app/src/main/java/com/example/android/photobyintent/PhotoIntentActivity.java
+++ LeafO3App/app/src/main/java/com/example/android/photobyintent/PhotoIntentActivity.java
@@ -28,18 +28,18 @@ import android.widget.VideoView;
 public class PhotoIntentActivity extends Activity {
 
 	private static final int ACTION_TAKE_PHOTO_B = 1;
-	private static final int ACTION_TAKE_PHOTO_S = 2;
-	private static final int ACTION_TAKE_VIDEO = 3;
+	//private static final int ACTION_TAKE_PHOTO_S = 2;
+	//private static final int ACTION_TAKE_VIDEO = 3;
 
 	private static final String BITMAP_STORAGE_KEY = "viewbitmap";
 	private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewvisibility";
 	private ImageView mImageView;
 	private Bitmap mImageBitmap;
 
-	private static final String VIDEO_STORAGE_KEY = "viewvideo";
-	private static final String VIDEOVIEW_VISIBILITY_STORAGE_KEY = "videoviewvisibility";
-	private VideoView mVideoView;
-	private Uri mVideoUri;
+//	private static final String VIDEO_STORAGE_KEY = "viewvideo";
+//	private static final String VIDEOVIEW_VISIBILITY_STORAGE_KEY = "videoviewvisibility";
+//	private VideoView mVideoView;
+//	private Uri mVideoUri;
 
 	private String mCurrentPhotoPath;
 
@@ -127,9 +127,9 @@ public class PhotoIntentActivity extends Activity {
 		
 		/* Associate the Bitmap to the ImageView */
 		mImageView.setImageBitmap(bitmap);
-		mVideoUri = null;
+		//mVideoUri = null;
 		mImageView.setVisibility(View.VISIBLE);
-		mVideoView.setVisibility(View.INVISIBLE);
+		//mVideoView.setVisibility(View.INVISIBLE);
 	}
 
 	private void galleryAddPic() {
@@ -166,18 +166,18 @@ public class PhotoIntentActivity extends Activity {
 		startActivityForResult(takePictureIntent, actionCode);
 	}
 
-	private void dispatchTakeVideoIntent() {
+	/*private void dispatchTakeVideoIntent() {
 		Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
 		startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
 	}
-
+*/
 	private void handleSmallCameraPhoto(Intent intent) {
 		Bundle extras = intent.getExtras();
 		mImageBitmap = (Bitmap) extras.get("data");
 		mImageView.setImageBitmap(mImageBitmap);
-		mVideoUri = null;
+		//mVideoUri = null;
 		mImageView.setVisibility(View.VISIBLE);
-		mVideoView.setVisibility(View.INVISIBLE);
+		//mVideoView.setVisibility(View.INVISIBLE);
 	}
 
 	private void handleBigCameraPhoto() {
@@ -191,10 +191,10 @@ public class PhotoIntentActivity extends Activity {
 	}
 
 	private void handleCameraVideo(Intent intent) {
-		mVideoUri = intent.getData();
-		mVideoView.setVideoURI(mVideoUri);
+		//mVideoUri = intent.getData();
+		//mVideoView.setVideoURI(mVideoUri);
 		mImageBitmap = null;
-		mVideoView.setVisibility(View.VISIBLE);
+		//mVideoView.setVisibility(View.VISIBLE);
 		mImageView.setVisibility(View.INVISIBLE);
 	}
 
@@ -206,21 +206,23 @@ public class PhotoIntentActivity extends Activity {
 		}
 	};
 
-	Button.OnClickListener mTakePicSOnClickListener = 
+	/*Button.OnClickListener mTakePicSOnClickListener =
 		new Button.OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			dispatchTakePictureIntent(ACTION_TAKE_PHOTO_S);
 		}
 	};
-
-	Button.OnClickListener mTakeVidOnClickListener = 
+*/
+/*
+	Button.OnClickListener mTakeVidOnClickListener =
 		new Button.OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			dispatchTakeVideoIntent();
 		}
 	};
+*/
 
 	/** Called when the activity is first created. */
 	@Override
@@ -229,9 +231,9 @@ public class PhotoIntentActivity extends Activity {
 		setContentView(R.layout.main);
 
 		mImageView = (ImageView) findViewById(R.id.imageView1);
-		mVideoView = (VideoView) findViewById(R.id.videoView1);
+		//mVideoView = (VideoView) findViewById(R.id.videoView1);
 		mImageBitmap = null;
-		mVideoUri = null;
+		//mVideoUri = null;
 
 		Button picBtn = (Button) findViewById(R.id.btnIntend);
 		setBtnListenerOrDisable( 
@@ -239,6 +241,7 @@ public class PhotoIntentActivity extends Activity {
 				mTakePicOnClickListener,
 				MediaStore.ACTION_IMAGE_CAPTURE
 		);
+/*
 
 		Button picSBtn = (Button) findViewById(R.id.btnIntendS);
 		setBtnListenerOrDisable( 
@@ -246,14 +249,17 @@ public class PhotoIntentActivity extends Activity {
 				mTakePicSOnClickListener,
 				MediaStore.ACTION_IMAGE_CAPTURE
 		);
+*/
 
+/*
 		Button vidBtn = (Button) findViewById(R.id.btnIntendV);
 		setBtnListenerOrDisable( 
 				vidBtn, 
 				mTakeVidOnClickListener,
 				MediaStore.ACTION_VIDEO_CAPTURE
 		);
-		
+*/
+
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
 			mAlbumStorageDirFactory = new FroyoAlbumDirFactory();
 		} else {
@@ -271,7 +277,7 @@ public class PhotoIntentActivity extends Activity {
 			break;
 		} // ACTION_TAKE_PHOTO_B
 
-		case ACTION_TAKE_PHOTO_S: {
+		/*case ACTION_TAKE_PHOTO_S: {
 			if (resultCode == RESULT_OK) {
 				handleSmallCameraPhoto(data);
 			}
@@ -283,7 +289,7 @@ public class PhotoIntentActivity extends Activity {
 				handleCameraVideo(data);
 			}
 			break;
-		} // ACTION_TAKE_VIDEO
+		}*/ // ACTION_TAKE_VIDEO
 		} // switch
 	}
 
@@ -291,9 +297,9 @@ public class PhotoIntentActivity extends Activity {
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
-		outState.putParcelable(VIDEO_STORAGE_KEY, mVideoUri);
+//		outState.putParcelable(VIDEO_STORAGE_KEY, mVideoUri);
 		outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null) );
-		outState.putBoolean(VIDEOVIEW_VISIBILITY_STORAGE_KEY, (mVideoUri != null) );
+//		outState.putBoolean(VIDEOVIEW_VISIBILITY_STORAGE_KEY, (mVideoUri != null) );
 		super.onSaveInstanceState(outState);
 	}
 
@@ -301,17 +307,17 @@ public class PhotoIntentActivity extends Activity {
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
 		mImageBitmap = savedInstanceState.getParcelable(BITMAP_STORAGE_KEY);
-		mVideoUri = savedInstanceState.getParcelable(VIDEO_STORAGE_KEY);
+		//mVideoUri = savedInstanceState.getParcelable(VIDEO_STORAGE_KEY);
 		mImageView.setImageBitmap(mImageBitmap);
 		mImageView.setVisibility(
 				savedInstanceState.getBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY) ? 
 						ImageView.VISIBLE : ImageView.INVISIBLE
 		);
-		mVideoView.setVideoURI(mVideoUri);
+		/*mVideoView.setVideoURI(mVideoUri);
 		mVideoView.setVisibility(
 				savedInstanceState.getBoolean(VIDEOVIEW_VISIBILITY_STORAGE_KEY) ? 
 						ImageView.VISIBLE : ImageView.INVISIBLE
-		);
+		);*/
 	}
 
 	/**
